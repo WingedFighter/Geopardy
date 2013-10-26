3501bb53510add89d072a1180a41aa803991079a
KeanuBlee <keanu.b.lee@gmail.com>
2013-10-26 02:23:02 -0400

    woo

--- randEncounter.py
+++ randEncounter.py
@@ -10,10 +10,8 @@ def lineSort(rawr):
 	random_line_num = random.randrange(0, len(lines))
 	oList.append(lines[random_line_num])
 	V = oList[0]
-	splitOList = V.split()
-	randomEncounter = random.choice(splitOList)
-	rawr2.write(randomEncounter)
-	return(random.choice(splitOList))
+	rawr2.write(V)
+	return(random.choice(V))
 lineSort(rawr)
 rawr.close()
 rawr2.close()
