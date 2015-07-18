fcf95d00f53acf163ba1609e9f4ebc3a778e676e
Henry Peteet <hvpeteet@gmail.com>
2015-07-18 16:15:58 -0700

    Made basic fixes
    fixed this so that it now works :)
    TODO(hvpeteet): make word stripping better and answer recognition
    better.

--- geopardy.py
+++ geopardy.py
@@ -20,13 +20,7 @@ from google.appengine.ext import ndb
 LOADER = jinja2.FileSystemLoader(os.path.dirname(__file__))
 jinja_environment = jinja2.Environment(loader = LOADER)
 
-  
-numSearchTerms = 10
-currentKeys = 0
-keys = []
 lines = []
-start = time.clock()
-refreshTimer = 100
 
 rawr = open('searchTerms.txt','r')   
 
@@ -36,37 +30,28 @@ for x in range(0, len(lines)):
     lines[x] = lines[x].strip()
 rawr.close()
 
-
-
 class Search(ndb.Model):
     searchBlocks = ndb.JsonProperty()
     answer = ndb.StringProperty()
 
-def getSearch():
+def getSearch(searchword=None):
+  if searchword == None:
+    searchword = random.choice(lines)
+  s = ndb.Key("Search", searchword).get()
+  if s == None:
     try:
-        i = random.choice(keys)
-        return i.get()
-    except IndexError:
-        createSearch()
-        return getSearch()
+      createSearch(searchword)
+      return getSearch(searchword)
+    except Exception:
+      return Search.query().fetch(1)[0]
+  else:
+    return s
     
 
-def createSearch():
-    global currentKeys
-    searchword = lines[currentKeys]
+def createSearch(searchword):
     try :
-        # logging.info("----------------------------------------------------------------")
-        # logging.info(searchword);
         url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=%r&start=1&rsz=8" % searchword
-        # logging.info(url)
-        # logging.info("----------------------------------------------------------------")
         response = urlfetch.fetch(url)
-        # logging.info(type(response))
-        # logging.info("----------------------------------------------------------------")
-        # logging.info("----------------------------------------------------------------")
-        # logging.info(response.content)
-        # logging.info("----------------------------------------------------------------")
-        # logging.info("----------------------------------------------------------------")
         data = json.loads(response.content)
         responseData = data["responseData"]
         results = responseData["results"]
@@ -76,71 +61,38 @@ def createSearch():
             content.append(results[i]["content"])
         
         s = Search(searchBlocks = removeSearchTerm(content, searchword), answer = searchword)
-        s_key = s.put()
-        keys.append(s_key)
-        currentKeys += 1
-
-    except TypeError:
-        logging.info("JSON issue")
-        pass
+        s.key = ndb.Key("Search", searchword)
+        s.put()
+    except Exception as e:
+        logging.info("Search issue" + str(e))
+        raise e
 
 
 def removeSearchTerm(listOfInfo, searchword):
-    for info in range(len(listOfInfo)):
-        if searchword.lower() in listOfInfo[info].lower():
-            listOfInfo[info] = listOfInfo[info].lower().replace(searchword.lower(), "_____")
-            # logging.info("************************************************")
-            # logging.info("Claims replacement")
-            # logging.info("************************************************")
+    for info in xrange(len(listOfInfo)):
+      listOfInfo[info] = listOfInfo[info].lower().replace(searchword.lower(), "_____")
     return listOfInfo
 
 
 class HomeHandler(webapp2.RequestHandler):   
     def get(self):
-        #Get Values
-        #Modify Values
-        global start
-        logging.info("==========================================================")
-        logging.info(time.clock()-start)
-        logging.info("==========================================================")
-        if(time.clock() - start > refreshTimer):
-            createSearch()
-            start = time.clock()
-
         search = getSearch()
         correct = search.answer
         content = search.searchBlocks
         template_values = {"content" : content, 
                             "correct" : correct
                             }
-        #Write Values
         template = jinja_environment.get_template('templates/home.html')
         self.response.out.write(template.render(template_values))
 
+
+class ResultHandler(webapp2.RequestHandler):
     def post(self):
         answer = self.request.get("answer")
         correct = self.request.get("correct")
-        logging.info("Answer : ")
-        logging.info(answer)
-        logging.info("Correct : ")
-        logging.info(correct)
-        logging.info("NumKeys : ")
-        logging.info(len(keys))
-        if(answer.lower() == correct.lower()):
-            logging.info("We won")
-
-
-class ResultHandler(webapp2.RequestHandler):   
-    def get(self):
-        self.response.write("hello")
-    def post(self):
-        answer = self.request.get("answer")
-        correct = self.request.get("correct")
-        logging.info(len(keys))
         template_values = {"correct" : correct,
                             "answer" : answer,
                             }
-
         template = jinja_environment.get_template('templates/results.html')
         self.response.out.write(template.render(template_values))
 
